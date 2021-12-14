bf3991c56a4c3502ea15f70a12d4139a77eb473d
Olexandr <a.savelev.krm@gmail.com>
2021-12-14 15:43:09 +0200

    add building block preloader

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 const building = document.getElementById('building')
 const image = building.querySelector('.building__image')
+const preloader = document.getElementById('preloader')
 
 image.onload = function () {
+  preloader.style.display = 'none';
   building.style.display = 'block';
 }
